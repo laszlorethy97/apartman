cb33a8fafa491fe30ee1683b1c2a4ac6e242ffd9
lrethy <laszlorethy97@gmail.com>
2026-07-09 15:58:31 +0200

    init basic route

--- frontend/apartman-handler/src/app/app.routes.ts
+++ frontend/apartman-handler/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { CommonComponent } from './components/common-component/common-component';
+import { LoginComponent } from './components/login-component/login-component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: CommonComponent,
+        children: [
+            {
+                path: '',
+                component: LoginComponent,
+            },
+        ]
+    }
+];
---
+++ frontend/apartman-handler/src/app/components/common-component/common-component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-common-component',
+  imports: [RouterOutlet],
+  templateUrl: './common-component.html',
+  styleUrl: './common-component.scss',
+})
+export class CommonComponent {}
